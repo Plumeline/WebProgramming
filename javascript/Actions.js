45ab37adc5c1fd03b827df1ad027307f99f11683
Plumeline <151142975+Plumeline@users.noreply.github.com>
2025-06-02 18:43:45 +0200

    comments

--- javascript/Actions.js
+++ javascript/Actions.js
@@ -2,14 +2,25 @@
 const factor_vaccine = 3;
 const factor_well = 0.75;
 
+//this file allows the gift part of the file to work
+//when increasing the amount of money to give, this file will update the number of people helped displayed
+// when making a gift, it will display a thank you message
+//the data of how many people are helped will also be added to the table of statistics
+// ex if you give 1$, it will pass from 0 person helped to 3 and 0.75,
+//and when you press "give" it will effectively add those 3 and 0.75 to the fields corresponding in the table of data
+// this table and the data inside it are saved in local storage ; reloading the page won't reset the data
 
-window.
 
+
+//window.
+
+//onload, charge data
 window.onload = function(){
 
     loadData();
 };
 
+//on unload, save data
 window.addEventListener("beforeunload", function(){
 
     saveData();
@@ -17,10 +28,13 @@ window.addEventListener("beforeunload", function(){
 
 });
 
+
+//function to deal with the change of money
 function increase(money){
 
-    console.log("something happens.");
+    //console.log("something happens.");
 
+    //we get the necessary informations for this
     let input = document.getElementById("money");
     let persons = document.getElementById("nbchildren");
     let vaccine = document.getElementById("nbvaccine");
@@ -29,15 +43,18 @@ function increase(money){
 
     let curr = parseInt(input.value) ;
 
+
+    //if money is 0 and we try to go negative, not possible, stop function
     if(money < 0 && curr ===0){
         return;
     }
 
+    //increase the number of money
     curr = curr+ money;
 
     input.value= curr;
 
-
+    //update the number of people helped depending on how many money is given 
     persons.textContent= curr * factor_well;
 
     vaccine.textContent = curr * factor_vaccine;
@@ -45,32 +62,36 @@ function increase(money){
 
 }
 
-
+//function to deal with event : button 'gift is pressed'
 function gift(){
 
 
+    //we get how many is being given
     let input = document.getElementById("money");
 
     let money = parseInt(input.value);
 
+    // check if the amount given is not zero
     if(money===0){
         alert("You need at least 1 dollar to make a donation");
         return;
     }
 
+    
     let vaccine = document.getElementById("vaccine");
 
     let well = document.getElementById("well");
 
 
-
+    //add to the table the amount of persons helped by the gift
     vaccine.textContent = parseInt(vaccine.textContent) + money *factor_vaccine;
 
     well.textContent = parseInt(well.textContent) + money *factor_well;
 
+    //and we decrease the money by its amount to get back to zero
     increase(-money);
 
-
+    //display a little message to thank the giver
     alert("Thank you a lot for your gift of " + money + " $, many people are grateful for your help !");
 
 
@@ -79,7 +100,7 @@ function gift(){
 
 
 
-
+//function to save data in local storage
 function saveData() {
 
     const vaccine = document.getElementById("vaccine").textContent;
@@ -93,7 +114,7 @@ function saveData() {
 }
 
 
-
+//function to load data from local storage
 function loadData() {
 
     const vaccine = localStorage.getItem("vaccineCount");
--- javascript/Crew.js
+++ javascript/Crew.js
@@ -1,11 +1,23 @@
 //localStorage.clear();
 
 
+//this file is dedicated to the connection system of the crew page. 
+//you can create an account, and register. Your email and passwords are saved so you can connect again
+//you can also disconnect from the page using sign out button.
+//You can connect back to any account you created using your email and password corresponding to that account.
+//reloading the page does NOT disconnect you from the page (local storage), only sign out button does
+// creating a new account will increment the number of contributors
+
+
+
+//these are the elementary operations to be done when page loads
 window.onload = function () {
+
+    // we check if user is connected using the function
     loadConnection();
 
 
-    //update nb of contributors
+    //we update the number of contributors if it has been increased.
     let contributor = document.getElementById("nbcontributors");
     let storedValue = localStorage.getItem("contributors");
     contributor.textContent = storedValue !== null ? storedValue : "17000";
@@ -18,6 +30,7 @@ window.onload = function () {
 function subscribe(){
 
     
+    //first we get all the data given by the user
     var firstname = document.getElementById("first_name");
     var lastname = document.getElementById("last_name");
     var collaborator = document.getElementById("collaborator");
@@ -27,6 +40,7 @@ function subscribe(){
     var password = document.getElementById("password");
     var confirmation = document.getElementById("confirmation");
 
+    //we check if any of the field is empty
     if(firstname.value===""){
         alert("Please enter your First Name");
         return;
@@ -62,13 +76,16 @@ function subscribe(){
         return;
     }
 
-    //console.log("value of email :" + email.value);
+
+
+    //we use the dedicated function to check if user is 18+ 
     if(isOver18(birthdate) === false){
 
         alert("Sorry ! You can only register if you are 18+ ! Come back when you are older !");
         return;
     }
 
+    //we check whether or not the password and password confirmation match
     if(password.value != confirmation.value){
 
 
@@ -79,6 +96,7 @@ function subscribe(){
         return;
     }
 
+    //we check if the email isn't already used for another account.
     if(localStorage.getItem(email.value) !== null){
         alert("email has already been used for an account ! ");
         return;
@@ -86,12 +104,15 @@ function subscribe(){
 
     
 
-    log(firstname.value);
+    //log(firstname.value);
     
+
+    //we add to the storage this combination to let us know we are connected to this account.
     localStorage.setItem("connected", firstname.value);
+    //we save the combination of informations about user that is to be used for connection.
     localStorage.setItem(email.value, JSON.stringify([password.value, firstname.value]));
 
-
+    //we reset every field
     firstname.value="";
     lastname.value="";
     collaborator.value="";
@@ -100,7 +121,7 @@ function subscribe(){
     password.value="";
     confirmation.value="";
 
-
+    //since a new contributor has joined, we increase the number of contributors, and save the new value 
     let contributors = document.getElementById("nbcontributors");
     let newvalue = parseInt(contributors.textContent);
     newvalue ++;
@@ -130,7 +151,7 @@ function isOver18(date){
 }
 
 
-
+//this function is for updating the page visuals when we are connected. (simply to avoid repetitions in different functions)
 function log(username){
 
     
@@ -153,19 +174,21 @@ function log(username){
 
 
 
-// function to check if user is connected when first connection happens ----------------------------------------
+// function to check if user is connected when loading the page ----------------------------------------
 function loadConnection(){
 
+    //check if there is currently a user connected
     if ("connected" in localStorage){
     }
     else{
         return;
+        //if not end function
     }
 
 
     const username = localStorage.getItem("connected");
 
-
+    //use the function to make the page display accordingly (make the necessary visual changes for the page to show)
     log(username);
 
 
@@ -178,33 +201,43 @@ function loadConnection(){
 //function to sign in with an existing account -----------------------------------------------------
 function sign_in(){
 
+    //we get email and password given by user
     let email = document.getElementById("email_sign_in");
 
     let password = document.getElementById("password_sign_in");
 
+    // we check if an account exists with this mail
     if(localStorage.getItem(email.value) !== null){
 
     }
     else{
+        //if not, we return an error to the user, and end function
+
         alert("There is no account associated to this email. Please check if email is correct, or subscribe ");
         return;
     }
 
+    //if an account is linked to this mail, we load the informations linked to it
 
     var userdata = JSON.parse(localStorage.getItem(email.value));
 
+    // we check if the password given corresponds to the password linked to this account
     if(userdata[0] === password.value){
 
 
-        log(userdata[1]);
+        //log(userdata[1]);
 
+
+        //if yes the user can connect, we store the state connected to the local storage
         localStorage.setItem("connected", userdata[1]);
 
+        //we reset input fields
         email.value="";
         password.value="";
 
     }
     else{
+        //if passwords don't match, we ask user to try again and end function
         alert("Wrong password, please try again ! ");
         return;
     }
@@ -220,9 +253,10 @@ function sign_in(){
 // function to sign out when connected ----------------------------------
 function sign_out(){
 
-
+    //remove the state connected so the page doesn't connect again when reloading page
     localStorage.removeItem("connected");
 
+    //we do all the necessary visual changes to display back sign and log forms. (and reset personnalized user page)
     let form = document.getElementById("inscription");
     let sign= document.getElementById("sign_in");
     let welcome = document.getElementById("welcome");
